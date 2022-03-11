d610fab2ffd0fb91bf1b1e332e8818d1e40c6c2e
tkikn0211 <kn.tki211@gmail.com>
2022-03-11 10:24:55 +0000

    マイページと編集ページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,30 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+     @user = User.find(params[:id])
   end
-  
+
+  def edit
+    @user = current_user
+    redirect_to user_path(current_user) unless current_user == @user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to user_path(current_user)
+    else
+      render :show
+    end
+  end
+
+   private
+
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :address, :email, :account_name, :user_image_id, :hobby, :body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,21 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users
   root to: "homes#top"
   get "home_about"=>"homes#about"
-  resources :users, omly: [:show, :edit]
+  resources :users, omly: [:show, :edit, :update] do
+    collection do
+      get 'unsubscribe'
+      patch 'withdraw'
+    end
+  end
   resources :recruitments, only: [:new, :index, :show, :edit, :create]
   resources :genres, only: [:index]
   resources :profiles, only: [:show]
   resources :notifications, only: [:index]
   resources :messages, only:[:index, :show, :create]
 
-  
-  
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
