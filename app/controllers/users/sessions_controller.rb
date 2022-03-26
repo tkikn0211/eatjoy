d18214dcb1addaf371aa5d62ea7162cbfffbb4b4
tkikn0211 <kn.tki211@gmail.com>
2022-03-26 13:40:29 +0000

    デプロイ修正

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,7 +4,7 @@ class Users::SessionsController < Devise::SessionsController
 
   #ゲストログイン
   def new_guest
-    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+    user = User.find_or_create_by!(email:'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64 #ランダムのパスワード作成
       #NN制約のためデータを渡す
       user.last_name = "ゲスト"
