af9eb1314520698d99a633e73938e39ebeaa1551
tkikn0211 <kn.tki211@gmail.com>
2022-03-26 13:21:57 +0000

    デプロイ修正

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -38,4 +38,91 @@ RSpec.describe "Users", type: :system do
       end
     end
   end
+  
+  describe 'ユーザーログイン' do
+      before do
+        visit new_user_session_path
+      end
+      
+      ontext 'ログイン画面に遷移' do
+        it 'ログインに成功する' do
+          fill_in "メールアドレス", with: user.email
+          fill_in 'パスワード', with: user.password
+          click_button 'ログイン'
+          expect(page).to have_content 'ログインしました。'
+        end
+        
+        it 'ログインに失敗する' do
+          fill_in 'メールアドレス', with: ''
+          fill_in 'パスワード', with: ''
+          click_button 'ログイン'
+          expect(current_path).to eq new_user_session_path
+        end
+      end
+    end
+  end
+  
+  describe '編集のテスト' do
+      context '編集画面へ遷移' do
+        it '遷移ができる' do
+          visit edit_user_path(user)
+          expect(current_path).to eq edit_user_path(user)
+        end
+      end
+      
+      context '表示の確認と編集' do
+        before do
+          visit edit_user_path(user)
+        end
+        it '登録情報編集と表示される' do
+          expect(page).to have_content('登録情報編集')
+        end
+        it '画像編集フォームが表示される' do
+          expect(page).to have_field 'user[user_image]'
+        end
+        it '名前編集フォームに自分の姓が表示される' do
+          expect(page).to have_field '名前(姓)', with: user.last_name
+        end
+        it '名前編集フォームに自分の名が表示される' do
+          expect(page).to have_field '名前(名)', with: user.first_name
+        end
+        it '名前編集フォームに自分のカナ姓が表示される' do
+          expect(page).to have_field 'フリガナ(セイ)', with: user.last_name_kana
+        end
+        it '名前編集フォームに自分のカナ名が表示される' do
+          expect(page).to have_field 'フリガナ(メイ)', with: user.first_name_kana
+        end
+        it 'メールアドレス編集フォームに自分のメールアドレスが表示される' do
+          expect(page).to have_field 'メールアドレス', with: user.email
+        end
+        it '住所編集フォームに自分の住所が表示される' do
+          expect(page).to have_field '住所', with: user.address
+        end
+        it "趣味編集フォームに自分の趣味が表示される" do
+          expect(page).to have_field "趣味", with: user.hobby
+        end
+        it '自己紹介編集フォームに自分の自己紹介文が表示される' do
+          expect(page).to have_field '自己紹介', with: user.body
+        end
+        it '編集に成功する' do
+          # 名前を三郎に変更
+          fill_in '名前(名)', with: '三郎'
+          fill_in 'フリガナ(メイ)', with: 'サブロウ'
+          click_button '変更を保存する'
+          expect(page).to have_content '会員情報の更新が完了しました。'
+          expect(page).to have_content 'テスト 三郎 (テスト サブロウ)'
+          expect(current_path).to eq user_path(user)
+        end
+        it '編集に失敗する' do
+          # first_name 名前(名)を空欄で入力
+          fill_in '名前(名)', with: ''
+          click_button '変更を保存する'
+          expect(page).to have_content '件のエラーが発生したため 個人会員 は保存されませんでした。'
+          expect(page).to have_content '名前(名)を入力してください'
+          expect(current_path).to eq user_path(user)
+        end
+      end
+    end
+  end
+  
 end
