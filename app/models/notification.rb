f2a0216ccd8da98d4755c6d467efc3fddbdbbf55
tkikn0211 <kn.tki211@gmail.com>
2022-03-27 05:26:02 +0000

    デプロイ修正

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,8 +1,8 @@
 class Notification < ApplicationRecord
 
   default_scope -> { order(created_at: :desc) }
-  
-  belongs_to :recruitment, optional: true
+
+  belongs_to :recruitment, optional: true, foreign_key: "recruitment_id"
   belongs_to :message, optional: true
   belongs_to :visitor, class_name: "User", foreign_key: "visitor_id", optional: true
   belongs_to :visited, class_name: "User", foreign_key: "visited_id", optional: true
